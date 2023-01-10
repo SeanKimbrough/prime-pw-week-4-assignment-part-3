9a6e24b0d5cc8e5f5e64e8fd934606c0e03ed718
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-01-10 15:06:03 -0600

    finished question 1

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,14 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
-// We want to see how you are testing your code!!!
+// We want to see how you are testing your code!!
+
+//////////////////////////////////////////////////////////////////
+let basket = [];
+function addItem(item){
+    let newItem = basket.push(item)
+    return true;
+}
+
+function listItems(){
+    for
+}
