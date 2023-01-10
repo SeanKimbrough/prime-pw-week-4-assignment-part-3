61c5f753cc61a96602eecc897e72f15f7bcb9ae3
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-01-10 16:51:36 -0600

    added console functions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -10,5 +10,16 @@ function addItem(item){
 }
 
 function listItems(){
-    for
+    for (let i=0; i < basket.length; i++){
+        const individualValue = basket[i];
+        console.log('individual item' , individualValue);
+    }
 }
+function empty(){
+    basket = [];
+}
+console.log(empty(basket));
+console.log('basket is:' , basket);
+console.log('Adding Pineapple, expect true' , addItem('Pineapple'));
+
+
